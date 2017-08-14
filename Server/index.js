5cd3683f61b572c214594cf044369c681e683522
Cibin <cibinmohan@gmail.com>
2017-08-14 17:55:16 +1000

    initiaal one

---
+++ Server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import path from'path';
+let app = express();
+
+app.get('/*',(req,res)=>{
+res.sendFile(path.join(__dirname,'./index.html'));
+});
+
+app.listen(3000, () => console.log('listen on ser 3000'));
