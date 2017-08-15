75ec3de3201ca27fc8650016d2acb9abd17613b8
Cibin <cibinmohan@gmail.com>
2017-08-15 19:35:48 +1000

    bootstrap loaded

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
+import NavigationBar from './NavigationBar';
+
 class App extends React.Component{
 render(){
     return(
         <div className="container">
-           <h1>Start</h1>
-            {this.props.children}                        
-            <Link to = "signup">Signup</Link>
-            <Link to = "greet">Greet</Link>
+            <NavigationBar/>
+            {this.props.children}  
         </div>
     );
 }
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -3,12 +3,23 @@ import { Link} from 'react-router-dom';
 class NavigationBar extends React.Component{
     render(){
     return(  
-        <div>
-            <h1>Start</h1>
-            {this.props.children}                        
-            <Link to = "signup">Signup</Link>
-            <Link to = "greet">Greet</Link>
-        </div>
+            <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                <a className="navbar-brand" href="#">Styx Football</a>
+                </div>
+                <ul className="nav navbar-nav">
+                <li className="active"><Link to = "greet">Greet</Link></li>
+                <li><Link to = "greet">Greet</Link></li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                <li>
+                <Link to = "signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+                <li><Link to = "signup"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
+                </ul>
+            </div>
+            </nav>
+
     );
     }
 }
